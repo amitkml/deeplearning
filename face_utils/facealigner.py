a41e689ac963b3ab0c7c69c14487b68b121a0a8f
AmitK <49492030+amitkml@users.noreply.github.com>
2019-10-19 22:38:24 +0530

    Add files via upload

--- face_utils/facealigner.py
+++ face_utils/facealigner.py
@@ -1,10 +1,32 @@
 # import the necessary packages
-import helpers
-from helpers import FACIAL_LANDMARKS_68_IDXS
-from helpers import FACIAL_LANDMARKS_5_IDXS
+# import helpers
+# from helpers import FACIAL_LANDMARKS_68_IDXS
+# from helpers import FACIAL_LANDMARKS_5_IDXS
 from helpers import shape_to_np
 import numpy as np
 import cv2
+
+# define a dictionary that maps the indexes of the facial
+# landmarks to specific face regions
+
+#For dlib’s 68-point facial landmark detector:
+FACIAL_LANDMARKS_68_IDXS = OrderedDict([
+	("mouth", (48, 68)),
+	("inner_mouth", (60, 68)),
+	("right_eyebrow", (17, 22)),
+	("left_eyebrow", (22, 27)),
+	("right_eye", (36, 42)),
+	("left_eye", (42, 48)),
+	("nose", (27, 36)),
+	("jaw", (0, 17))
+])
+
+#For dlib’s 5-point facial landmark detector:
+FACIAL_LANDMARKS_5_IDXS = OrderedDict([
+	("right_eye", (2, 3)),
+	("left_eye", (0, 1)),
+	("nose", (4))
+])
 
 class FaceAligner:
 	def __init__(self, predictor, desiredLeftEye=(0.35, 0.35),
